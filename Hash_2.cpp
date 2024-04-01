d7f7bcf898caf44aa026020eeeb47bb424de13c6
Dinar Jahan <110754416+DinarPerseus@users.noreply.github.com>
2024-04-01 20:14:55 +0600

    hash calculation for full string

---
+++ Hash_2.cpp
@@ -0,0 +1,51 @@
+// Dinar Perseüs 2.0
+#include <bits/stdc++.h>
+using namespace std;
+
+
+const int N = 200005;
+const long long prime = 31;
+const int mod = 1e9+9;
+long long p[N];
+
+//prime power calculation with memoization
+long long prime_power(int n){
+    if(p[n]!=0) return p[n];
+    if(n>0){
+        return p[n]=(prime_power(n-1)*prime)%mod;
+    }else {
+        return p[n]=1;
+    }
+}
+
+
+
+//hash calculation for each index of a string
+long long Hash(string &s){
+    
+    
+    int n = s.size();
+    long long hash_value=0;
+    
+    for(int i=0;i<n;i++){
+        hash_value=(hash_value+(s[i]-'a'+1)*prime_power(i))%mod;
+    }
+    
+    return hash_value;
+
+}
+
+
+
+
+
+
+
+int main()
+{
+    string s="dinar";
+    cout<<Hash(s);
+
+
+    return 0;
+}
